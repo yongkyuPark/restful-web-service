6746e0c9d1a2a6ddc12ad0f0c9e1c3cf36ad988d
Yongkyu Park <dydrbdjdsla@gmail.com>
2023-11-03 23:14:31 +0900

    feat : Filtering 추가

--- src/main/java/com/example/restfulwebservice/user/User.java
+++ src/main/java/com/example/restfulwebservice/user/User.java
@@ -1,5 +1,6 @@
 package com.example.restfulwebservice.user;
 
+import com.fasterxml.jackson.annotation.JsonFilter;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.validation.constraints.Past;
@@ -11,7 +12,8 @@ import java.util.Date;
 
 @Data
 @AllArgsConstructor
-@JsonIgnoreProperties(value={"password", "ssn"})
+//@JsonIgnoreProperties(value={"password", "ssn"})
+@JsonFilter("UserInfo")
 public class User {
     private Integer id;
 
