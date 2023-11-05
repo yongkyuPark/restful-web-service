e6578e4da9d3b8a64e5cdd6ee7d40a72f2f460b1
Yongkyu Park <dydrbdjdsla@gmail.com>
2023-11-05 23:52:02 +0900

    feat : jpa 추가

--- src/main/java/com/example/restfulwebservice/user/User.java
+++ src/main/java/com/example/restfulwebservice/user/User.java
@@ -3,6 +3,7 @@ package com.example.restfulwebservice.user;
 import com.fasterxml.jackson.annotation.JsonFilter;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.*;
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
@@ -10,13 +11,19 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.util.Date;
+import java.util.List;
 
 @Data
 @AllArgsConstructor
 //@JsonIgnoreProperties(value={"password", "ssn"})
 @NoArgsConstructor
 //@JsonFilter("UserInfo")
+@Entity
+@Table(name = "TS_USER")
 public class User {
+
+    @Id
+    @GeneratedValue
     private Integer id;
 
     @Size(min=2, message = "Name은 2글자 이상 입력해 주세요.")
@@ -28,4 +35,16 @@ public class User {
     private String password;
     //@JsonIgnore
     private String ssn;
+
+    @OneToMany(mappedBy = "user")
+    private List<Post> posts;
+
+
+    public User(int id, String name, Date joinDate, String password, String ssn) {
+        this.id = id;
+        this.name = name;
+        this.joinDate = joinDate;
+        this.password = password;
+        this.ssn = ssn;
+    }
 }
---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+insert into TS_USER values (900001, now(), 'User1', 'test1111', '701010-1111111');
+insert into TS_USER values (900002, now(), 'User2', 'test2222', '801010-2222222');
+insert into TS_USER values (900003, now(), 'User3', 'test3333', '901010-1111111');
+
+insert into TS_POST values (10001, 900001, 'test1');
+insert into TS_POST values (10002, 900001, 'test2');
